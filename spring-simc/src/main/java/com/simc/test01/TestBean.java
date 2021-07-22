2e1b049414b389608c21831e21eb09961e2be5c5
simc <simingchao@wanmagroup.com>
2021-07-22 17:35:25 +0800

    测试

--- spring-simc/src/main/java/com/simc/test01/TestBean.java
+++ spring-simc/src/main/java/com/simc/test01/TestBean.java
@@ -1,8 +1,19 @@
 package com.simc.test01;
 
+
 public class TestBean {
 
+	private String userName;
+
+	public void setUserName(String userName){
+		this.userName = userName;
+	}
+
+	public String getUserName(){
+		return this.userName;
+	}
+
 	public void aaa(){
-		System.out.println("nihao...");
+		System.out.println("nihao..."+this.userName);
 	}
 }
